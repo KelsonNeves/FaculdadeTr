a26ea8a60f1541e7eff3a67233703cce4c3463cb
KelsonNeves <kel.neves0@gmail.com>
2025-05-28 11:07:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+let posicao = 200;
+let posicao2 = 200;
+let ativo = 0;
+let intervalId = null;
+
+const Personagem = document.getElementById("Personagem");
+const BtEsquerda = document.getElementById("BtEsquerda");
+const BtDireita = document.getElementById("BtDireita");
+const mover = document.getElementById("Cmaior");
+
+BtEsquerda.addEventListener("mousedown", function(){
+    clearInterval(intervalId);
+    iresquerda();
+    intervalId = setInterval(iresquerda, 100);
+});
+BtEsquerda.addEventListener("mouseup", parar);
+BtEsquerda.addEventListener("mouseleave", parar);
+
+BtDireita.addEventListener("mousedown", function(){
+    clearInterval(intervalId);
+    irdireita();
+    intervalId = setInterval(irdireita, 100);
+});
+BtDireita.addEventListener("mouseup", parar);
+BtDireita.addEventListener("mouseleave", parar);
+
+mover.addEventListener("mousemove", function(e){
+    posicao2 +=1;
+    mover.style.left = (e.clientX - 20)+ "px";
+
+    //mover.style.left = e.clientx + "px";
+});
+
+function iresquerda() {
+    posicao -= 10;  
+    Personagem.style.left = posicao + 'px';
+}
+function irdireita() {
+    posicao += 10;  
+    Personagem.style.left = posicao + 'px';
+}
+
+function parar(){
+    clearInterval(intervalId);
+
+}
+
