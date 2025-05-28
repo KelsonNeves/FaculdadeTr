e280966212157c4fde9b7fe634f92c554cf2c50d
KelsonNeves <kel.neves0@gmail.com>
2025-05-28 16:01:40 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,69 +1,128 @@
-let posicao = 200;
-let posicao2 = 200;
-let ativo = 0;
-let intervalId = null;
+
 
 const Personagem = document.getElementById("Personagem");
 const BtEsquerda = document.getElementById("BtEsquerda");
 const BtDireita = document.getElementById("BtDireita");
-const mover = document.getElementById("Cmaior");
+const mover = document.getElementById("Cmenor");
+const moverArea = document.getElementById("Cmaior");
+const tela = document.getElementById("Tela");
+
+let TamanhoTelax = tela.offsetWidth;
+let TamanhoTelay = tela.offsetHeight;
+let TamanhomoverAreax = moverArea.offsetWidth;
+let TamanhomoverAreay = moverArea.offsetHeight;
+let Tamanhomoverx = mover.offsetWidth;
+let Tamanhomovery = mover.offsetHeight;
+let posicaox = tela.offsetWidth/2;
+let posicaoy = tela.offsetHeight/2;
+let movX = null;
+let movY = null;
+let VaoMov = 10;
+
+
+moverArea.style.left = (TamanhoTelax/2)-(TamanhomoverAreax/2)+ +(Tamanhomoverx/2) +"px";
+moverArea.style.top = tela.offsetHeight + "px";
+mover.addEventListener("touchmove", function(e){
+    
+        e.preventDefault();
+        const touch = e.touches[0];
+
+        if(touch.clientX <= ((TamanhoTelax/2)+(TamanhomoverAreax/2)+(Tamanhomoverx/2)) &&
+        touch.clientX >= ((TamanhoTelax/2)-(TamanhomoverAreax/2)+(Tamanhomoverx/2))){
+
+            mover.style.left = (touch.clientX - (TamanhoTelax/2) + (TamanhomoverAreax/2))- (Tamanhomoverx) + "px";
+        }
+        if(touch.clientY <= ((TamanhoTelay+TamanhomoverAreay)) &&
+        touch.clientY >= ((TamanhoTelay))){
+            mover.style.top = touch.clientY - TamanhoTelay - (Tamanhomovery/2)+ "px";
+        }
+//movimento horizontal
+        if(parseInt(mover.style.left)<= ((TamanhomoverAreax/2)-(Tamanhomoverx/2)-VaoMov) &&
+        parseInt(mover.style.left) >= (0-(TamanhomoverAreax/2)+(Tamanhomoverx/2))){
+            pararX();
+            iresquerda();
+            movX = setInterval(iresquerda, 4);
+
+
+        }if(parseInt(mover.style.left)<= (TamanhomoverAreax) &&
+        parseInt(mover.style.left) >= ((TamanhomoverAreax/2)-(Tamanhomoverx/2)+VaoMov)){
+            pararX();
+            irdireita();
+            movX = setInterval(irdireita, 4);
 
 
 
+        }if(parseInt(mover.style.left)< ((TamanhomoverAreax/2)-(Tamanhomoverx/2)+VaoMov) &&
+        parseInt(mover.style.left) > ((TamanhomoverAreax/2)-(Tamanhomoverx/2)-VaoMov)){
+            pararX();
+
+        }
 
 
-BtEsquerda.addEventListener("mousedown", function(){
-    clearInterval(intervalId);
-    iresquerda();
-    intervalId = setInterval(iresquerda, 100);
+//movimento vertical
+
+
+        if(parseInt(mover.style.top)<= ((TamanhomoverAreay/2)-(Tamanhomovery/2)-VaoMov) &&
+        parseInt(mover.style.top) >= (0-(TamanhomoverAreay/2)+(Tamanhomovery/2))){
+            pararY();
+            irCima();
+            movY = setInterval(irCima, 4);
+
+
+        }if(parseInt(mover.style.top)<= (TamanhomoverAreay) &&
+        parseInt(mover.style.top) >= ((TamanhomoverAreay/2)-(Tamanhomovery/2)+VaoMov)){
+            pararY();
+            irBaixo();
+            movY = setInterval(irBaixo, 4);
+
+
+        }if(parseInt(mover.style.top)<= ((TamanhomoverAreay/2)-(Tamanhomovery/2)-VaoMov) &&
+        parseInt(mover.style.top) >= ((TamanhomoverAreay/2)-(Tamanhomovery/2)+VaoMov)){
+            pararY();
+        }
+        
+    
+
 });
-BtEsquerda.addEventListener("mouseup", parar);
-BtEsquerda.addEventListener("mouseleave", parar);
 
-BtDireita.addEventListener("mousedown", function(){
-    clearInterval(intervalId);
-    irdireita();
-    intervalId = setInterval(irdireita, 100);
+mover.addEventListener("touchend", function(){
+    mover.style.left = ((TamanhomoverAreax/2)-(Tamanhomoverx/2))+"px" ;
+    mover.style.top = ((TamanhomoverAreay/2)-(Tamanhomovery/2))+"px" ;
+    pararX();
+    pararY();
 });
-BtDireita.addEventListener("mouseup", parar);
-BtDireita.addEventListener("mouseleave", parar);
 
-mover.addEventListener("mousemove", function(e){
-    posicao2 +=1;
-    mover.style.left = (e.clientX - 20)+ "px";
-
-    //mover.style.left = e.clientx + "px";
-});
-BtEsquerda.addEventListener("touchstart", function(e){
-    clearInterval(intervalId);
-    e.preventDefault();
-    iresquerda();
-    intervalId = setInterval(iresquerda, 100);
-});
-BtEsquerda.addEventListener('touchend', parar);
-BtDireita.addEventListener("touchstart", function(e){
-    clearInterval(intervalId);
-    e.preventDefault();
-    irdireita();
-    intervalId = setInterval(irdireita, 100);
-});
-BtDireita.addEventListener('touchend', parar);
 
 
 
 
 
 function iresquerda() {
-    posicao -= 10;  
-    Personagem.style.left = posicao + 'px';
+    posicaox -= 1;  
+    Personagem.style.left = posicaox + 'px';
 }
 function irdireita() {
-    posicao += 10;  
-    Personagem.style.left = posicao + 'px';
+
+    posicaox += 1;  
+    Personagem.style.left = posicaox + 'px';
+}
+function irCima() {
+
+    posicaoy -= 1;  
+    Personagem.style.top = posicaoy + 'px';
+}
+function irBaixo() {
+
+    posicaoy += 1;  
+    Personagem.style.top = posicaoy + 'px';
 }
 
-function parar(){
-    clearInterval(intervalId);
-
+function pararX(){
+    clearInterval(movX);
+    movX = null;
+}
+function pararY(){
+    clearInterval(movY);
+    movY = null;
 }
 
